d33fda65542ec664543ff551c522a4e0db00e457
amine-ben <amine.benallam@gmail.com>
2018-08-29 12:56:30 +0200

    update prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,12 @@ WORKING_DIR=`pwd`
 MAVEN_INDEXER=$WORKING_DIR/maven-miner-parent/maven-miner-indexer
 MAVEN_MINER=$WORKING_DIR/maven-miner-parent/maven-miner-aether
 FILES=$WORKING_DIR/files
+MAVEN_INDEXER_DIR=$FILES/maven-indexer
+MAVEN_MINER_DIR=$FILES/maven-miner
+
+mkdir $MAVEN_MINER_DIR
+mkdir $MAVEN_INDEXER_DIR
+
 echo "Removing existing jars"
 INDEXER_JAR=target/maven-miner-indexer-*-jar-with-dependencies.jar
 AETHER_JAR=target/maven-miner-aether-*-jar-with-dependencies.jar
@@ -13,9 +19,9 @@ rm -f $AETHER_JAR
 echo "Building maven-miner Indexer"
 cd $MAVEN_INDEXER
 mvn clean package
-mv -f $INDEXER_JAR $FILES/maven-indexer/maven-miner-indexer.jar
+mv -f $INDEXER_JAR $MAVEN_INDEXER_DIR/maven-miner-indexer.jar
 
 echo "Building maven-miner Indexer"
 cd $MAVEN_MINER
 mvn clean package
-mv -f $AETHER_JAR $FILES/maven-miner/maven-miner-aether.jar
+mv -f $AETHER_JAR $MAVEN_MINER_DIR/maven-miner-aether.jar
