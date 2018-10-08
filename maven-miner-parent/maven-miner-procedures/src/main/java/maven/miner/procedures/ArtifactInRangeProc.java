7b19b1c9fea338c68d3cda0b5e7c47cb9c87c16f
amine-ben <amine.benallam@gmail.com>
2018-10-08 13:47:04 +0200

    adding ranking and temporal slincing procedures

--- maven-miner-parent/maven-miner-procedures/src/main/java/maven/miner/procedures/ArtifactInRangeProc.java
+++ maven-miner-parent/maven-miner-procedures/src/main/java/maven/miner/procedures/ArtifactInRangeProc.java
@@ -1,5 +1,6 @@
 package maven.miner.procedures;
 
+import java.time.LocalDate;
 import java.util.stream.Stream;
 
 //import org.neo4j.graphdb.Direction;
@@ -23,28 +24,13 @@ import maven.miner.output.OutputNode;
 public class ArtifactInRangeProc extends AbstractProcedureEnv {
 	
 	/**
-	 * 
 	 * This procedure returns all latest artifacts
 	 */
 	@Procedure(value="maven.miner.artifacts.during", mode = Mode.READ)
 	@Description("retrieve all latest artifacts in a particular range")
 	public Stream<OutputNode> getArtifactsInRange(@Name("Deployment year") long year, 
 			  									  @Name(value= "Deployment month", defaultValue = "0") long month) 
-	{	
-		
-			// Retrieving the calendar node corresponding to the appropriate year 
-//			Node yearNode = graphDB.findNode(calendarLabel, Properties.YEAR, year);
-			
-//			if (yearNode == null) return Stream.empty();
-//			result = graphDB .traversalDescription()
-//					.depthFirst()
-//					.evaluator(path -> { return Evaluation.of(path.endNode().hasLabel(Label.label(groupName)), false);})
-//					.relationships(DependencyRelation.YEAR, Direction.INCOMING)
-//					.uniqueness(Uniqueness.NODE_GLOBAL)
-//					.traverse(yearNode)
-//					.nodes()
-//					.stream()
-//		
+	{		
 		return getArtifactsInRange(Properties.ARTIFACT_LABEL, year, month);
 	}
 	
@@ -96,6 +82,62 @@ public class ArtifactInRangeProc extends AbstractProcedureEnv {
 		return result;
 	}
 	
+	/**
+	 * 
+	 * This procedure returns all latest artifacts
+	 */
+	@Procedure(value="maven.miner.artifacts.between", mode = Mode.READ)
+	@Description("retrieve all latest artifacts in a particular range")
+	public Stream<OutputNode> getArtifactsInPeriod(
+			@Name("start date") LocalDate startDate, 
+			@Name(value= "end date", defaultValue = "9999-12-31") LocalDate endDate) 
+	{
+		return getArtifactsInPeriod(Properties.ARTIFACT_LABEL, startDate, endDate);
+	}
+	/**
+	 * This procedure returns all latest artifacts
+	 */
+	@Procedure(value="maven.miner.artifacts.group.between", mode = Mode.READ)
+	@Description("retrieve all latest artifacts in a particular range")
+	public Stream<OutputNode> getArtifactsInPeriod(@Name(value = "Group name") String groupName,
+												   @Name("start date") LocalDate startDate, 
+												   @Name(value= "end date", defaultValue = "9999-12-31") LocalDate endDate) 
+	{
+		int startYear = startDate.getYear();
+		int startMonth = startDate.getMonthValue();
+		
+		int endYear = endDate.getYear();
+		int endMonth = endDate.getMonthValue();
+		
+		Stream<OutputNode> result = null;
+		try (Transaction tx = graphDB.beginTx()) {
+			String query = String.format("match (m)<-[:MONTH]-(n:`%s`)-[:YEAR]->(y) \n" + 
+											"where (y.year = %d and m.month > %d) \n" + 
+											"or y.year> %d\n" + 
+											"or y.year< %d \n" + 
+											"or (y.year = %d and m.month < %d)\n" + 
+											"return n",
+											groupName,
+											startYear,
+											startMonth,
+											startYear,
+											endYear,
+											endYear,
+											endMonth);
+			
+			Result queryResult = graphDB.execute(query);
+			result = queryResult.columnAs("n")
+								.stream()
+								.map(node -> new OutputNode((Node)node));
+			tx.success();
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw e;	
+		}
+		return result;
+	}
+	
+	
 	@SuppressWarnings("unused")
 	private class YearMonthEvaluator implements Evaluator {
 
@@ -109,3 +151,19 @@ public class ArtifactInRangeProc extends AbstractProcedureEnv {
 	}
 
 }
+
+// The traversal API is supposed to be faster then CypHer QUERIES, HOWEVER, NOT BENIFITING FROM ACCESS TO INDEX MAY SLOW DOWN THE QUERY
+// Retrieving the calendar node corresponding to the appropriate year 
+//Node yearNode = graphDB.findNode(calendarLabel, Properties.YEAR, year);
+
+//if (yearNode == null) return Stream.empty();
+//result = graphDB .traversalDescription()
+//		.depthFirst()
+//		.evaluator(path -> { return Evaluation.of(path.endNode().hasLabel(Label.label(groupName)), false);})
+//		.relationships(DependencyRelation.YEAR, Direction.INCOMING)
+//		.uniqueness(Uniqueness.NODE_GLOBAL)
+//		.traverse(yearNode)
+//		.nodes()
+//		.stream()
+//
+
---
+++ maven-miner-parent/maven-miner-procedures/src/main/java/maven/miner/procedures/ArtifactsRankingProc.java
@@ -0,0 +1,62 @@
+package maven.miner.procedures;
+
+import java.util.stream.Stream;
+
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Result;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Mode;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import fr.inria.diverse.maven.common.Properties;
+import maven.miner.output.OutputNode;
+
+public class ArtifactsRankingProc extends AbstractProcedureEnv {
+	
+	@Procedure(value="maven.miner.artifacts.top", mode = Mode.READ)
+	@Description("top X most used artifacts")
+	public Stream<OutputNode> getTopArtifacts(
+			@Name(value = "the number to limit the top too")long top,
+			@Name(value = "dependency scope. none by default", defaultValue = "any") String scope) {
+		return getTopArtifacts(Properties.ARTIFACT_LABEL, top, scope);
+	}
+
+	public Stream<OutputNode> getTopArtifacts(
+			@Name("the groupName")String artifactLabel, 
+			@Name(value = "the number to limit the top too")long top,
+			@Name(value = "dependency scope. none by default", defaultValue = "any") String scope) {
+
+		Stream<OutputNode> result = null;
+		String scopeS = null;
+		if (scope.equals("Runtime")
+				|| scope.equals("Compile")
+				|| scope.equals("Test")
+				|| scope.equals("Provided")) {
+			scopeS = String.format("{scope : '%s' }",scope);
+		} else if (!scope.equalsIgnoreCase("any")) {
+			throw new RuntimeException("Supported scopes are: Runtime, Compile, Provided, "
+										+ "and Test. To refer  to all scopes use 'any'!");
+		}
+		try (Transaction tx = graphDB.beginTx()) {
+			String query = String.format("match (n:`%s`) \n" + 
+										 "with n, size(()-[:DEPENDS_ON %s]->(n)) as sizes\n" + 
+										 "return n order by sizes DESC limit %d",
+										 artifactLabel,
+										 scopeS,
+										 top
+										 );
+			Result queryResult = graphDB.execute(query);
+			result = queryResult.columnAs("n")
+								.stream()
+								.map(node -> new OutputNode((Node)node));
+			tx.success();
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw e;	
+		}
+		return result;
+	}
+
+}
