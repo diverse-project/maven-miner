48435f5bd46efb93b0379b51794ea2cad486dbca
amine-ben <amine.benallam@gmail.com>
2018-10-12 21:04:36 +0200

    Adding similarity ficntions tests and fixing Neo4j types to Java types issues

--- maven-miner-parent/maven-miner-procedures/src/main/java/maven/miner/functions/SimilarityFunc.java
+++ maven-miner-parent/maven-miner-procedures/src/main/java/maven/miner/functions/SimilarityFunc.java
@@ -1,6 +1,7 @@
 package maven.miner.functions;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 
@@ -14,63 +15,65 @@ import info.debatty.java.stringsimilarity.interfaces.StringSimilarity;
 
 public class SimilarityFunc {
 	 
-	 @UserFunction(name = "maven.miner.dependencies.similarity.cosine")
-	 @Description("maven.miner.dependencies.similarity.cosine(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> cosineSimilarity(@Name("oldDependencies") String [] removedDependencies, 
-			 									@Name("newDependencies") String [] newDependencies) {
+	 @UserFunction(name = "maven.miner.similarity.cosine")
+	 @Description("maven.miner.similarity.cosine(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Map<String, Map<String,Number>> cosineSimilarity(@Name("oldDependencies") List<String> removedDependencies, 
+			 									@Name("newDependencies") List<String> newDependencies) {
 		 Cosine similarity = new Cosine();
 		 return computeSimilarity(removedDependencies, newDependencies, similarity::similarity);
 	 }
-	 @UserFunction(name = "maven.miner.dependencies.similarity.cosine.single")
-	 @Description("maven.miner.dependencies.similarity.cosine.single(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> cosineSimilarity(@Name("oldDependencies") String  removedDependency, 
-			 									@Name("newDependencies") String  newDependency) {
+	 @UserFunction(name = "maven.miner.similarity.cosine.single")
+	 @Description("maven.miner.similarity.cosine.single(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Number cosineSimilarity(@Name("first") String  first, 
+			 									@Name("second") String  second) {
 		 Cosine similarity = new Cosine();
-		 return computeSimilarity(new String[] {removedDependency},new String[] {newDependency}, similarity::similarity);
+		 return similarity.similarity(first, second);
 	 }
-	 @UserFunction(name = "maven.miner.dependencies.similarity.cosine.custom")
-	 @Description("maven.miner.dependencies.similarity.cosine.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> cosineCustomSimilarity(@Name("oldDependencies")String [] removedDependencies, 
-			 										@Name("newDependencies") String [] newDependencies) {
+	 @UserFunction(name = "maven.miner.similarity.cosine.custom")
+	 @Description("maven.miner.similarity.cosine.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Map<String, Map<String,Number>> cosineCustomSimilarity(@Name("oldDependencies")List<String> removedDependencies, 
+			 										@Name("newDependencies") List<String> newDependencies) {
 		 CustomSimilarity similarity = new CustomSimilarity( new Cosine());
 		 return computeSimilarity(removedDependencies, newDependencies, similarity::similarity);
 	 }
-	 @UserFunction(name = "maven.miner.dependencies.similarity.cosine.custom.single")
-	 @Description("maven.miner.dependencies.similarity.cosine.custom.single(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> cosineCustomSimilarity(@Name("oldDependencies")String removedDependencies, 
-			 										@Name("newDependencies") String  newDependencies) {
+	 @UserFunction(name = "maven.miner.similarity.cosine.custom.single")
+	 @Description("maven.miner.similarity.cosine.custom.single(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Number cosineCustomSimilarity(@Name("oldDependencies")String first, 
+			 										@Name("newDependencies") String  second) {
 		 CustomSimilarity similarity = new CustomSimilarity( new Cosine());
-		 return computeSimilarity(new String[] {removedDependencies}, new String[] {newDependencies}, similarity::similarity);
-	 }
+		 return similarity.similarity(first, second);
+		 }
 	 
-	 @UserFunction(name = "maven.miner.dependencies.similarity.jaroWinkler")
-	 @Description("maven.miner.dependencies.similarity.jaroWinkler(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> jaroWinklerSimilarity(@Name("oldDependencies")String [] removedDependencies, 
-			 										@Name("newDependencies")	String [] newDependencies) {
+	 @UserFunction(name = "maven.miner.similarity.jaroWinkler")
+	 @Description("maven.miner.similarity.jaroWinkler(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Map<String, Map<String,Number>> jaroWinklerSimilarity(@Name("oldDependencies")List<String> removedDependencies, 
+			 										@Name("newDependencies")	List<String> newDependencies) {
 		 JaroWinkler similarity = new JaroWinkler();
 		 return computeSimilarity(removedDependencies, newDependencies, similarity::similarity);
 	 }
-	 @UserFunction(name = "maven.miner.dependencies.similarity.jaroWinkler.custom")
-	 @Description("maven.miner.dependencies.similarity.jaroWinkler.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> jaroWinklerCustomSimilarity(@Name("oldDependencies")String [] removedDependencies, 
-			 											@Name("newDependencies") String [] newDependencies) {
+	 @UserFunction(name = "maven.miner.similarity.jaroWinkler.custom")
+	 @Description("maven.miner.similarity.jaroWinkler.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Map<String, Map<String,Number>> jaroWinklerCustomSimilarity(@Name("oldDependencies")List<String> removedDependencies, 
+			 											@Name("newDependencies") List<String> newDependencies) {
 		 CustomSimilarity similarity = new CustomSimilarity( new JaroWinkler());
 		 return computeSimilarity(removedDependencies, newDependencies, similarity::similarity);
 	 }
 
-	 @UserFunction(name = "maven.miner.dependencies.similarity.jaroWinkler.single")
-	 @Description("maven.miner.dependencies.similarity.jaroWinkler.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> jaroWinklerSimilarity(@Name("oldDependencies")String  removedDependencies, 
-			 											@Name("newDependencies") String  newDependencies) {
+	 @UserFunction(name = "maven.miner.similarity.jaroWinkler.single")
+	 @Description("maven.miner.similarity.jaroWinkler.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Number jaroWinklerSimilarity(@Name("first")String  first, 
+			 											@Name("second") String  second) {
 		 JaroWinkler similarity = new JaroWinkler();
-		 return computeSimilarity(new String[] {removedDependencies}, new String[] {newDependencies}, similarity::similarity);
+		 return similarity.similarity(first, second);
+
 	 }
-	 @UserFunction(name = "maven.miner.dependencies.similarity.jaroWinkler.custom.single")
-	 @Description("maven.miner.dependencies.similarity.jaroWinkler.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
-	 public Map<String, Map<String,Number>> jaroWinklerCustomSimilarity(@Name("oldDependencies")String removedDependencies, 
-			 											@Name("newDependencies") String newDependencies) {
+	 @UserFunction(name = "maven.miner.similarity.jaroWinkler.custom.single")
+	 @Description("maven.miner.similarity.jaroWinkler.custom(['d1','d2',...], ['d1','d2',...]) - Computes the similarity matrix.")
+	 public Number jaroWinklerCustomSimilarity(@Name("first")String first, 
+			 											@Name("second") String second) {
 		 CustomSimilarity similarity = new CustomSimilarity( new JaroWinkler());
-		 return computeSimilarity(new String[] {removedDependencies}, new String[] {newDependencies}, similarity::similarity);
+		 return similarity.similarity(first, second);
+
 	 }
 	 /**
 	  * 
@@ -79,15 +82,15 @@ public class SimilarityFunc {
 	  * @param similarityFunc
 	  * @return
 	  */
-	 private Map<String, Map<String,Number>> computeSimilarity (String [] removedDependencies, 
-			    String [] newDependencies, BiFunction<String, String, Number> similarityFunc) {	
+	 private Map<String, Map<String,Number>> computeSimilarity (List<String> removedDependencies, 
+			    List<String> newDependencies, BiFunction<String, String, Number> similarityFunc) {	
 		 
 			Map<String, Map<String,Number>> result = createMap(removedDependencies, newDependencies);
 			try {
-				for (int i = 0; i < removedDependencies.length; i++) {
-					for (int j = 0; j < newDependencies.length; j++) {
-					String first = removedDependencies[i];
-					String second = newDependencies [j];
+				for (int i = 0; i < removedDependencies.size(); i++) {
+					for (int j = 0; j < newDependencies.size(); j++) {
+					String first = removedDependencies.get(i);
+					String second = newDependencies.get(j);
 					result.get(first).put(second, similarityFunc.apply(first, second));		
 					}
 				}
@@ -102,14 +105,14 @@ public class SimilarityFunc {
 	  * @param newDependencies
 	  * @return
 	  */
-	 private static Map<String, Map<String, Number>> createMap(String[] removedDependencies, 
-														String[] newDependencies) {
+	 private static Map<String, Map<String, Number>> createMap(List<String> removedDependencies, 
+			 List<String>  newDependencies) {
 		Map<String, Map<String,Number>> result = new HashMap<String, Map<String,Number>>();
-		for (int i = 0; i < removedDependencies.length; i++) {
+		for (int i = 0; i < removedDependencies.size(); i++) {
 			Map<String,Number> row = new HashMap<String, Number>();
-			result.put(removedDependencies[i], row);
-			for (int j = 0; j < newDependencies.length; j++ ) {
-				row.put(newDependencies[j],0);
+			result.put(removedDependencies.get(i), row);
+			for (int j = 0; j < newDependencies.size(); j++ ) {
+				row.put(newDependencies.get(j),0);
 			}
 		}
 		return result;
