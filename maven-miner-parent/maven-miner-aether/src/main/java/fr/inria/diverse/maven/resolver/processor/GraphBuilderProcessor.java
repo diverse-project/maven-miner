3822a8608da05c47e41979e492f31d7b3eb9bac8
nharrand <nicolas.harrand@gmail.com>
2019-09-04 16:35:02 +0200

    add new mm resolver

---
+++ maven-miner-parent/maven-miner-aether/src/main/java/fr/inria/diverse/maven/resolver/processor/GraphBuilderProcessor.java
@@ -0,0 +1,82 @@
+package fr.inria.diverse.maven.resolver.processor;
+
+import fr.inria.diverse.maven.model.Edge;
+import fr.inria.diverse.maven.resolver.db.Neo4jGraphDBWrapper;
+import fr.inria.diverse.maven.resolver.tasks.Neo4jGraphDependencyVisitorTask;
+import fr.inria.diverse.maven.util.MavenMinerUtil;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.graph.Dependency;
+import org.sonatype.aether.resolution.ArtifactDescriptorException;
+import org.sonatype.aether.resolution.ArtifactDescriptorRequest;
+import org.sonatype.aether.resolution.ArtifactDescriptorResult;
+
+import javax.validation.constraints.NotNull;
+
+public class GraphBuilderProcessor extends AbstractArtifactProcessor {
+	protected Neo4jGraphDBWrapper dbWrapper;
+
+
+	public GraphBuilderProcessor(Neo4jGraphDBWrapper dbWrapper) {
+		this.dbWrapper = dbWrapper;
+	}
+
+	protected int skipped = 0;
+
+	protected int collected = 0;
+
+	@Override
+	public Artifact process(Artifact artifact) {
+
+		LOGGER.info("Collecting dependencies for artifact: " + artifact);
+
+		ArtifactDescriptorRequest r = new ArtifactDescriptorRequest();
+		r.setArtifact(artifact);
+		r.addRepository(repo);
+		try {
+			ArtifactDescriptorResult result = system.readArtifactDescriptor(session,r);
+
+
+
+			dbWrapper.createNodeFromArtifactCoordinate( result.getArtifact());
+
+
+			for (Dependency dependency : result.getDependencies()) {
+				dbWrapper.createNodeFromArtifactCoordinate(dependency.getArtifact());
+				Edge.Scope scope = MavenMinerUtil.deriveScope(dependency);
+				dbWrapper.addDependency(result.getArtifact(), dependency.getArtifact(), scope);
+			}
+
+
+			collected++;
+			return result.getArtifact();
+		} catch (ArtifactDescriptorException e) {
+			LOGGER.error("Unable to collect dependency for artifact {}", artifact);
+			skipped++;
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public void report() {
+		LOGGER.info("{} artifacts have been collected", collected);
+		LOGGER.info("{} artifacts have failed collection", skipped);
+
+	}
+
+	/**
+	 *
+	 * @return dbWrapper {@link Neo4jGraphDBWrapper}
+	 */
+	public Neo4jGraphDBWrapper getDbWrapper() {
+		return dbWrapper;
+	}
+	/**
+	 *
+	 * @param dbWrapper {@link Neo4jGraphDBWrapper}
+	 */
+	public void setDbWrapper(Neo4jGraphDBWrapper dbWrapper) {
+		this.dbWrapper = dbWrapper;
+	}
+
+}
